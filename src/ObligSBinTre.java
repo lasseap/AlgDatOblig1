73a61a3ae7869b7b32335132d11d31fe1537fed2
AndreasGrymyr <44088424+andreasgrymyr@users.noreply.github.com>
2019-10-18 10:47:17 +0200

    Skisse av oppgave 4

--- src/ObligSBinTre.java
+++ src/ObligSBinTre.java
@@ -271,7 +271,50 @@ public class ObligSBinTre<T> implements Beholder<T>
 
   public String omvendtString()
   {
-    throw new UnsupportedOperationException("Ikke kodet ennå!");
+    Stack<Node> stack = new Stack<>();
+    String verdi;
+    Node temp;
+    Node forrige = null;
+    StringJoiner ut = new StringJoiner(", ", "[", "]");
+
+    stack.push(rot);
+
+    while(!stack.isEmpty()) {
+      Node current = stack.peek();
+
+      if(forrige == null || forrige.venstre == current || forrige.høyre == current ) {
+        if(current.venstre != null) {
+          stack.push(current.venstre);
+        }
+        else if(current.høyre!= null) {
+          stack.push(current.høyre);
+        }
+        else {
+          temp = stack.pop();
+          verdi = temp.verdi.toString();
+          ut.add(verdi);
+        }
+      }
+      else if(current.venstre == forrige) {
+        if(current.høyre != null) {
+          stack.push(current.høyre);
+        }
+        else {
+          temp = stack.pop();
+          verdi = temp.verdi.toString();
+          ut.add(verdi);
+        }
+      }
+      else if(current.høyre == forrige) {
+        temp = stack.pop();
+        verdi = temp.verdi.toString();
+        ut.add(verdi);
+      }
+
+      forrige = current;
+    }
+
+    return ut.toString();
   }
   
   public String høyreGren()
